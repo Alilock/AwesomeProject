dc02a9f7516cd8563e9d7e3fe4e3593a4dd4130b
Ali <105427281+Alilock@users.noreply.github.com>
2023-04-24 11:54:55 +0400

    newText

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Button, SafeAreaView, StyleSheet } from 'react-native'
 
 export default function App() {
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <View style={styles.styleView1}>
+        <Text style={styles.styleText1}>Rmad102</Text>
+        <Text style={styles.styleText1}>Rmad101</Text>
+      </View>
+      <View style={styles.styleView2}></View>
     </View>
-  );
+
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  styleView1: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "tomato",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 32,
   },
-});
+  styleText1: {
+    color: "black",
+    fontSize: 24,
+    fontWeight: "800"
+  },
+  styleView2: {
+    flex: 2, backgroundColor: "pink"
+  }
+
+
+})
+
+
+//flex 2 - 1 50%
+// View - div 
+//Text - p,span
+//Button - button
+// Flexbox - margin,padding, flex
